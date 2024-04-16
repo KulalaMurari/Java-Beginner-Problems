71fc1e6b78946644c1b1cce1835d9fc363f6c952
K Murari <97232079+KulalaMurari@users.noreply.github.com>
2024-04-16 05:48:49 +0530

    Add files via upload

---
+++ Addition of Two Num with Scanner/addition2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class addition2 {
+public static void main(String[] args){
+Scanner obj = new Scanner(System.in);
+int num1 = obj.nextInt();
+int num3 = obj.nextInt();
+int num2 = obj.nextInt();
+int result = num1 + num2 + num3;
+System.out.print("Result is:"+result);
+}
+}
